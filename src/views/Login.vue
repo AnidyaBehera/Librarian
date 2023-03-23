85a7e6677f37ae10ccd3de3e1bfd8f39d62b667a
Anidya Behera <beheraanidya@gmail.com>
2023-03-23 12:26:41 +0530

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <div class = "center">
+        <v-row>
+            <v-col cols = "10" offset = "1" sm="4" offset-sm="4" mr="3" offset-mr=3>
+                <v-avatar color="primary" size="100" tile>
+                    <v-img src="@/assets/logo.png"></v-img>
+                </v-avatar>
+                <v-text-field label="Username" v-model="username"></v-text-field>
+                <v-text-field label="Password" type="password" v-model="password"></v-text-field>
+                <v-btn block @click="authenticate()" color="light blue">Login</v-btn>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                username: "",
+                password: ""
+            }
+        },
+        methods: {
+            authenticate() {
+                alert("Login clicked")
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
